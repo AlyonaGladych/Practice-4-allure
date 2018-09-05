1faaa8d19d6367b75b59c55756ac38930d20bbc3
AlyonaGladych <41871016+AlyonaGladych@users.noreply.github.com>
2018-09-05 22:20:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,13 @@
 exports.config = {
     // The address of a running selenium server.
     seleniumAddress: 'http://localhost:4444/wd/hub',
+    SELENIUM_PROMISE_MANAGER: 0,
+    allScriptsTimeout: 30,
   
     // Capabilities to be passed to the webdriver instance.
     capabilities: {
-      browserName: 'chrome'
+      browserName: 'chrome',
+      maxInstances: 1
     },
   
     // Spec patterns are relative to the configuration file location passed
@@ -16,6 +19,24 @@ exports.config = {
     // Options to be passed to Jasmine-node.
     jasmineNodeOpts: {
       showColors: true, // Use colors in the command line report.
+      defaultTimeoutInterval: 45000
+    },
+
+    framework: 'jasmine2',
+
+    onPrepare: function() {
+      let AllureReporter = require('jasmine-allure-reporter');
+      jasmine.getEnv().addReporter(new AllureReporter({
+      resultsDir: 'allure-results'
+      }));
+      jasmine.getEnv().afterEach(function(done){
+        browser.takeScreenshot().then(function (png) {
+          allure.createAttachment('Screenshot', function () {
+            return new Buffer(png, 'base64')
+          }, 'image/png')();
+          done();
+        })
+      });
+
     }
   };
-  
