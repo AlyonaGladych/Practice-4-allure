588484731fa69ef95105e753dd3c025aee88cd2c
AlyonaGladych <41871016+AlyonaGladych@users.noreply.github.com>
2018-09-05 22:17:57 +0300

    Update demo_test.js

--- tests/demo_test.js
+++ tests/demo_test.js
@@ -2,9 +2,9 @@ let LoginPage = require("../page_objects/login_page.js");
 
 describe('login suite', function() {
     it('login pass test', async function() {
-      let loginPage = new LoginPage();
-
+      
       allure.createStep('login', function() {
+        let loginPage = new LoginPage();
         await loginPage.open();
         let productsPage = await loginPage.login("alyonagladych@gmail.com", "x&b*zmzA^oj");
       });  
